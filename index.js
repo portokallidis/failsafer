4d4a4abec711244d23d5df2b655cdf4cec0563c7
Nick Portokallidis <portokallidis@gmail.com>
2015-04-25 22:12:02 +0300

    add emailer and ssh exec script

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
-var express = require('express')
+var express = require('express');
+var exec = require('ssh-exec')
+var email = require('./email_sender');
 var app = express();
+app.set('port', 7999);
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+/*
+{ monitorID: '776793137',
+  monitorURL: 'http://beta.carre-project.eu:8080',
+  monitorFriendlyName: 'TESTHOST',
+  alertType: '2',
+  alertTypeFriendlyName: 'Up',
+  alertDetails: 'HTTP 200 - OK',
+  monitorAlertContacts: '2325540;5;http://beta.carre-project.eu:7999/carre?' }
+*/
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+app.get('/carre', function(req, res) {
+  console.log(req.query);
+  var id=req.query.monitorID;
+  var monitorURL=req.query.monitorURL;
+  var monitorFriendlyName=req.query.monitorFriendlyName;
+  var status=req.query.alertType; //1 or 2
+  //var alertDetails=req.params.alertDetails; //0 or 1
+  //monitorAlertContacts
+  var responseText='UpRobot : '+monitorFriendlyName + ' is'+(Number(status)===2?' up ':' down ');
+  console.log(responseText);
+  email.send();
+  res.send(responseText);
+  
+  
+});
+
+
+exec('list', 'dokku@athina.med.duth.gr').pipe(process.stdout)
+
+/*exec('list', {
+  user: 'dokku',
+  host: 'athina.med.duth.gr',
+  key: myKeyFileOrBuffer
+  
+}).pipe(process.stdout);*/
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
